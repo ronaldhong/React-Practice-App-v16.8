58bf481148610df237fecd073ce5ae208c2f4378
n221199515 <hong_ronald@hotmail.com>
2019-06-04 16:39:18 -0500

    handle error and conditional rendering

--- src/App.js
+++ src/App.js
@@ -40,52 +40,85 @@ class App extends React.Component{
   //super(props) inside the constructor if we have to use this.props, for example
   constructor(props){
     super(props);
-    this.state={lat: null};
-    window.navigator.geolocation.getCurrentPosition(
-      res=> {
-        console.log(res)
-        this.setState({
-          lat: res.coords.latitude
-        });
-      },
-      err=> {
-        console.log(err)
-      }
-    );
+    this.state={
+      lat: null,
+      errorMessage: ""
+    };
+  };
 
+
+  componentDidMount(){
+    console.log("In")
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+          this.setState({ lat: position.coords.latitude })
+        },
+        err => {
+          this.setState({ errorMessage: err.message })
+        }  
+    );
+    console.log(this.state)
+  };
+  
+  renderMessageBox() {
+    return (
+              <div>
+                <div className="ui container comments">
+                  <ApprovalCard>
+                    <div>
+                      <h4>Warning!</h4>
+                      Are you sure you want to do this?
+                    </div>
+                  </ApprovalCard>
+        
+                  <ApprovalCard>
+                    <CommentDetail 
+                      author ={faker.name.findName()} 
+                      content="Nice Post!"
+                      avatar = {faker.image.avatar()}
+                    />
+                  </ApprovalCard>
+                  <ApprovalCard>
+                    <CommentDetail author ={faker.name.findName()} content="Howdy!" avatar = {faker.image.avatar()} />
+                  </ApprovalCard>
+                  <ApprovalCard>
+                    <CommentDetail author ={faker.name.findName()} content="I love the weather!" avatar = {faker.image.avatar()} />
+                  </ApprovalCard>
+                </div>
+              </div>
+            )
   }
 
-  render(){
-    return (
-      <div>
-        <div>
-          Lattitude: {this.state.lat}
-        </div>
-        <div className="ui container comments">
-          <ApprovalCard>
-            <div>
-              <h4>Warning!</h4>
-               Are you sure you want to do this?
-            </div>
-          </ApprovalCard>
 
-          <ApprovalCard>
-            <CommentDetail 
-              author ={faker.name.findName()} 
-              content="Nice Post!"
-              avatar = {faker.image.avatar()}
-            />
-          </ApprovalCard>
-          <ApprovalCard>
-            <CommentDetail author ={faker.name.findName()} content="Howdy!" avatar = {faker.image.avatar()} />
-          </ApprovalCard>
-          <ApprovalCard>
-            <CommentDetail author ={faker.name.findName()} content="I love the weather!" avatar = {faker.image.avatar()} />
-          </ApprovalCard>
+  render(){
+    
+    if (this.state.errorMessage && !this.state.lat) {
+        console.log('1')
+        return (
+        <div>
+          <div>
+            Error: {this.state.errorMessage} 
+          </div>
+          {this.renderMessageBox()}
         </div>
-      </div>
+          )
+    }
+    if (this.state.lat && !this.state.errorMessage) {  
+        console.log('2')        
+        return (
+          <div>
+            <div>
+              Latitude: {this.state.lat}
+            </div>
+            {this.renderMessageBox()}
+          </div>
+            )
+    }
+    if (this.state.lat == null && this.state.errorMessage === ""){
+        console.log('3')
+        return <div>Loading!</div>
+    }
       
-    )
   }
 }
 
